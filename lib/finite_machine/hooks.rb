d7325d000cc2a9c673e02e638f06ab7f12df15ca
Piotr Murach <pmurach@gmail.com>
2014-03-15 22:20:05 +0000

    Add unregister and docs.

--- lib/finite_machine/hooks.rb
+++ lib/finite_machine/hooks.rb
@@ -10,6 +10,9 @@ module FiniteMachine
 
     # Initialize a collection of hoooks
     #
+    # @example
+    #   Hoosk.new(machine)
+    #
     # @api public
     def initialize(machine)
       @collection = Hash.new do |events_hash, event_type|
@@ -25,12 +28,42 @@ module FiniteMachine
     # @param [String] name
     # @param [Proc]   callback
     #
+    # @example
+    #   hooks.register :enterstate, :green do ... end
+    #
+    # @return [Hash]
+    #
     # @api public
     def register(event_type, name, callback)
       @collection[event_type][name] << callback
     end
 
+    # Unregister callback
+    #
+    # @param [String] event_type
+    # @param [String] name
+    # @param [Proc]   callback
+    #
+    # @example
+    #   hooks.unregister :enterstate, :green do ... end
+    #
+    # @return [Hash]
+    #
+    # @api public
+    def unregister(event_type, name, callback)
+      @collection[event_type][name].shift
+    end
+
     # Return all hooks matching event and state
+    #
+    # @param [String] event_type
+    # @param [String] event_state
+    # @param [Event] event
+    #
+    # @example
+    #   hooks.call(:entersate, :green, Event.new)
+    #
+    # @return [Hash]
     #
     # @api public
     def call(event_type, event_state, event)
