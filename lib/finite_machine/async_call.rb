1adf9f7bba410ba1d7fb0699119dcc363327ad4a
Piotr Murach <pmurach@gmail.com>
2014-03-29 20:50:59 +0000

    Add async call for event dispatch.

---
+++ lib/finite_machine/async_call.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+module FiniteMachine
+  # An asynchronouse call representation
+  class AsyncCall
+    include Threadable
+
+    attr_threadsafe :context
+
+    attr_threadsafe :callable
+
+    attr_threadsafe :arguments
+
+    attr_threadsafe :block
+
+    # Build asynchronous call instance
+    #
+    # @param [Object] context
+    # @param [Callable] callable
+    # @param [Array] args
+    # @param [#call] block
+    #
+    # @example
+    #   AsyncCall.build(self, Callable.new(:method), :a, :b)
+    #
+    # @return [self]
+    #
+    # @api public
+    def self.build(context, callable, *args, &block)
+      instance = new
+      instance.context = context
+      instance.callable = callable
+      instance.arguments = *args
+      instance.block = block
+      instance
+    end
+
+    # Dispatch the event to the context
+    #
+    # @return [nil]
+    #
+    # @api private
+    def dispatch
+      callable.call(context, *arguments, block)
+    end
+  end # AsyncCall
+end # FiniteMachine
