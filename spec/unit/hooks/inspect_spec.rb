9afaf07f872d2eb9af7300a4c8ab9befd9e18af2
Piotr Murach <pmurach@gmail.com>
2014-05-25 20:59:10 +0100

    Update hooks inspect test.

--- spec/unit/hooks/inspect_spec.rb
+++ spec/unit/hooks/inspect_spec.rb
@@ -3,15 +3,14 @@
 require 'spec_helper'
 
 describe FiniteMachine::Hooks, '#inspect' do
-  let(:machine) { double }
-
-  subject(:hooks) { described_class.new(machine) }
+  subject(:hooks) { described_class.new }
 
   it "displays name and transitions" do
     hook = -> { }
     event = FiniteMachine::HookEvent::Enter
     collection = {event => {yellow: [hook]}}
     hooks.register(event, :yellow, hook)
+
     expect(hooks.inspect).to eql("<#FiniteMachine::Hooks:0x#{hooks.object_id.to_s(16)} @collection=#{collection}>")
     expect(hooks.to_s).to eql(hooks.inspect)
   end
