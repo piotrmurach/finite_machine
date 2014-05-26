2361189b9e3be4bd3595b8c67b876eea6732dde6
Piotr Murach <pmurach@gmail.com>
2014-05-26 19:48:47 +0100

    Fix transition inspect.

--- spec/unit/transition/inspect_spec.rb
+++ spec/unit/transition/inspect_spec.rb
@@ -2,13 +2,13 @@
 
 require 'spec_helper'
 
-describe FiniteMachine::Transition, 'inspect' do
-  let(:machine) { double }
+describe FiniteMachine::Transition, '#inspect' do
+  let(:machine) { double(:machine) }
 
   subject(:transition) { described_class.new(machine, attrs) }
 
   context 'when inspecting' do
-    let(:attrs) { {name: :start, :foo => :bar, :baz => :daz} }
+    let(:attrs) { {name: :start, parsed_states: { :foo => :bar, :baz => :daz } } }
 
     it "displays name and transitions" do
       expect(transition.inspect).to eql("<#FiniteMachine::Transition @name=start, @transitions=foo -> bar, baz -> daz, @when=[]>")
@@ -16,7 +16,7 @@ describe FiniteMachine::Transition, 'inspect' do
   end
 
   context 'when converting to string' do
-    let(:attrs) { {name: :start, :foo => :bar } }
+    let(:attrs) { {name: :start, parsed_states: { :foo => :bar } } }
 
     it "displays name and transitions" do
       expect(transition.to_s).to eql("start")
