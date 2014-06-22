12f366128ade5045a90a871f93c7fa193cdb5405
Piotr Murach <pmurach@gmail.com>
2014-06-22 21:11:07 +0100

    Ensure correct states for conditional branching.

--- spec/unit/states_spec.rb
+++ spec/unit/states_spec.rb
@@ -16,6 +16,20 @@ describe FiniteMachine, 'states' do
       }
     end
 
-    expect(fsm.states).to eql([:none, :green, :yellow, :red])
+    expect(fsm.states).to match_array([:none, :green, :yellow, :red])
+  end
+
+  it "retrieves all unique states for choice transition" do
+    fsm = FiniteMachine.define do
+      initial :green
+
+      events {
+        event :next, from: :green do
+          choice :yellow, if: -> { false }
+          choice :red,    if: -> { true }
+        end
+      }
+    end
+    expect(fsm.states).to match_array([:none, :green, :yellow, :red])
   end
 end
