66f1258d2735fdf7963ab610dc1ac8676a80cbf4
Piotr Murach <pmurach@gmail.com>
2014-07-06 19:58:50 +0100

    Change spec helper configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,9 @@ end
 require 'finite_machine'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.raise_errors_for_deprecations!
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
