97e2a0904b1a6158b2485a60bbc952951c0ba87a
Piotr Murach <pmurach@gmail.com>
2014-08-02 21:25:50 +0100

    Change hook event equals and document behaviour.

--- lib/finite_machine/hook_event.rb
+++ lib/finite_machine/hook_event.rb
@@ -4,11 +4,12 @@ module FiniteMachine
   # A class responsible for event notification
   class HookEvent
     include Threadable
+    include Comparable
 
     MESSAGE = :trigger
 
-    # HookEvent state
-    attr_threadsafe :state
+    # HookEvent name
+    attr_threadsafe :name
 
     # HookEvent type
     attr_threadsafe :type
@@ -19,14 +20,33 @@ module FiniteMachine
     # Transition associated with the event
     attr_threadsafe :transition
 
-    def initialize(state, transition, *data, &block)
-      @state = state
+    # Instantiate a new HookEvent object
+    #
+    # @param [Symbol] name
+    #   The action or state name
+    # @param [FiniteMachine::Transition]
+    #   The transition associated with this event.
+    # @param [Array[Object]] data
+    #
+    # @example
+    #   HookEvent.new(:green, ...)
+    #
+    # @return [Object]
+    #
+    # @api public
+    def initialize(name, transition, *data)
+      @name       = name
       @transition = transition
       @data       = *data
       @type       = self.class
       freeze
     end
 
+    # Notify subscriber about this event
+    #
+    # @return [nil]
+    #
+    # @api public
     def notify(subscriber, *args, &block)
       if subscriber.respond_to? MESSAGE
         subscriber.public_send(MESSAGE, self, *args, &block)
@@ -49,13 +69,34 @@ module FiniteMachine
 
     EVENTS = Anystate, Enter, Transition, Exit, Anyaction, Before, After
 
+    # Extract event name
+    #
+    # @return [String] the event name
+    #
+    # @api public
     def self.event_name
       name.split('::').last.downcase.to_sym
     end
 
+    # String representation
+    #
+    # @return [String] the event name
+    #
+    # @api public
     def self.to_s
       event_name
     end
+
+    # Compare whether the instance is greater, less then or equal to other
+    #
+    # @return [-1 0 1]
+    #
+    # @api public
+    def <=>(other)
+      other.is_a?(type) && name == other.name &&
+        transition <=> other.transition && data <=> other.data
+    end
+    alias_method :eql?, :==
 
     EVENTS.each do |event|
       (class << self; self; end).class_eval do
---
+++ spec/unit/hook_event/eql_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe FiniteMachine, 'eql?' do
+  let(:name)       { :green }
+  let(:transition) { double(:transition) }
+  let(:data)       { [:foo, :bar] }
+  let(:object)     { FiniteMachine::HookEvent }
+
+  subject(:hook) { object.new(name, transition, *data) }
+
+  context 'with the same object' do
+   let(:other) { hook }
+
+    it "equals" do
+      expect(hook).to eql(other)
+    end
+  end
+
+  context 'with an equivalent object' do
+    let(:other) { hook.dup }
+
+    it "equals" do
+      expect(hook).to eql(other)
+    end
+  end
+
+  context "with an object having different name" do
+    let(:other_name) { :red }
+    let(:other) { object.new(other_name, transition, *data) }
+
+    it "doesn't equal" do
+      expect(hook).not_to eql(other)
+    end
+  end
+end
