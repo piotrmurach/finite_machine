d991a5a227a80ae27450dc52884c0d4c3f90adb4
Piotr Murach <pmurach@gmail.com>
2014-09-27 18:55:41 +0100

    Document mixin.

--- lib/finite_machine/two_phase_lock.rb
+++ lib/finite_machine/two_phase_lock.rb
@@ -1,14 +1,31 @@
+# encoding: utf-8
 
 module FiniteMachine
-  module TwoPhaseLock     
+  # Mixin to provide lock to a {Threadable}
+  #
+  # @api private
+  module TwoPhaseLock
+    # Create synchronization lock
+    #
+    # @return [Sync]
+    #
+    # @api private
     def sync
       @sync ||= Sync.new
     end
 
+    # Synchronize given block of code
+    #
+    # @param [Symbol] mode
+    #   the synchronization mode out of :SH and :EX
+    #
+    # @return [nil]
+    #
+    # @api private
     def synchronize(mode, &block)
       sync.synchronize(mode, &block)
     end
 
     module_function :sync, :synchronize
-  end
-end
+  end # TwoPhaseLock
+end # FiniteMachine
