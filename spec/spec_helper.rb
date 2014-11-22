3c19bdc3a7256ae26e17c80abacd40d79a75dbb5
Piotr Murach <pmurach@gmail.com>
2014-11-22 18:34:43 +0000

    Update specs helper to prevent monkey patching.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,12 +21,11 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.raise_errors_for_deprecations!
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.disable_monkey_patching!
+  config.order = :random
 
   # Remove defined constants
   config.before :each do
