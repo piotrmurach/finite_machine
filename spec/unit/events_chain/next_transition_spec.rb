8a8c5c43a7ea9dc3f5178d7b24e50238e955ad2a
Piotr Murach <pmurach@gmail.com>
2015-07-11 20:52:17 +0100

    Change tests to reflect changes.

--- spec/unit/events_chain/next_transition_spec.rb
+++ spec/unit/events_chain/next_transition_spec.rb
@@ -2,34 +2,32 @@
 
 require 'spec_helper'
 
-RSpec.describe FiniteMachine::Event, '#next_transition' do
-  let(:object) { described_class }
+RSpec.describe FiniteMachine::EventsChain, '.next_transition' do
+  let(:machine) { spy(:machine) }
 
-  subject(:event) { object.new(machine, name: :test) }
+  it "finds matching transition by name" do
+    transition_a = double(:transition_a, current?: false)
+    transition_b = double(:transition_b, current?: true)
+    event = FiniteMachine::Event.new(:go, machine)
+    event << transition_a
+    event << transition_b
 
-  describe "matches transition by name" do
-    let(:machine) { double(:machine) }
+    events_chain = described_class.new(machine)
+    events_chain.add(:go, event)
 
-    it "finds matching transition" do
-      transition_a = double(:transition_a, current?: false)
-      transition_b = double(:transition_b, current?: true)
-      event << transition_a
-      event << transition_b
-
-      expect(event.next_transition).to eq(transition_b)
-    end
+    expect(events_chain.next_transition(:go)).to eq(transition_b)
   end
 
-  describe "fails to find" do
-    let(:machine) { double(:machine) }
+  it "choses first available transition" do
+    transition_a = double(:transition_a, current?: false)
+    transition_b = double(:transition_b, current?: false)
+    event = FiniteMachine::Event.new(:go, machine)
+    event << transition_a
+    event << transition_b
 
-    it "choses first available transition" do
-      transition_a = double(:transition_a, current?: false)
-      transition_b = double(:transition_b, current?: false)
-      event << transition_a
-      event << transition_b
+    events_chain = described_class.new(machine)
+    events_chain.add(:go, event)
 
-      expect(event.next_transition).to eq(transition_a)
-    end
+    expect(events_chain.next_transition(:go)).to eq(transition_a)
   end
 end
