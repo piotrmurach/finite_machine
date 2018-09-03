f496a4f602ecd1f229602755fb0d1e88b79e9e67
Piotr Murach <pmurach@gmail.com>
2018-09-03 23:47:59 +0200

    Add filter

--- spec/performance/benchmark_spec.rb
+++ spec/performance/benchmark_spec.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-RSpec.describe FiniteMachine do
+RSpec.describe FiniteMachine, perf: true do
   include RSpec::Benchmark::Matchers
 
   class Measurement
