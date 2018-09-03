ee192f86cf987a99e230920a714edd264c944423
Piotr Murach <pmurach@gmail.com>
2018-09-03 22:39:47 +0200

    Change to load files directly

--- lib/finite_machine/async_proxy.rb
+++ lib/finite_machine/async_proxy.rb
@@ -1,4 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
+
+require_relative 'message_queue'
 
 module FiniteMachine
   # An asynchronous messages proxy
--- lib/finite_machine/dsl.rb
+++ lib/finite_machine/dsl.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 module FiniteMachine
   # A generic DSL for describing the state machine
